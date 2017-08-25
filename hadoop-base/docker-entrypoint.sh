1be4e57d3aa27b54fc06ff5d319c52ea6107316c
Wizard <lsw1991abc@gmail.com>
2017-08-25 21:43:54 +0800

    export JAVA_HOME

--- hadoop-base/docker-entrypoint.sh
+++ hadoop-base/docker-entrypoint.sh
@@ -1,17 +1,3 @@
 #!/bin/bash
 
-# 环境变量
-export JAVA_HOME=/usr/lib/jvm/java-1.8.0-openjdk-amd64 
-export HADOOP_HOME=/opt/$DISTRO_NAME 
-export HADOOP_MAPRED_HOME=$HADOOP_HOME 
-export HADOOP_COMMON_HOME=$HADOOP_HOME 
-export HADOOP_HDFS_HOME=$HADOOP_HOME 
-export YARN_HOME=$HADOOP_HOME 
-export HADOOP_COMMON_LIB_NATIVE_DIR=$HADOOP_HOME/lib/native 
-export HADOOP_INSTALL=$HADOOP_HOME
-
-export PATH=$PATH:$JAVA_HOME/bin:$HADOOP_HOME/sbin:$HADOOP_HOME/bin
-
-/etc/init.d/ssh start  -d
-
 exec "$@"
